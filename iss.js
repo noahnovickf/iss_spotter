6e39bf445de5d23ddf811679cf08c5b9806870ed
NoahNovick <nnovickfaille1@gmail.com>
2020-02-13 17:15:27 -0500

    iss spotter, coordinates found

---
+++ iss.js
@@ -0,0 +1,35 @@
+const request = require("request");
+
+const fetchMyIP = callback => {
+  request("https://api.ipify.org/?format=json", (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data.ip);
+    }
+  });
+};
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
